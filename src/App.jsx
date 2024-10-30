a71757d4eccbd93d6f55ba9bec6c8f2ae2f45a1e
yuriy05 <dilayyura@gmail.com>
2024-10-30 22:58:28 +0200

    added react query to handle remote state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import GlobalStyles from "./styles/GlobalStyles";
 
 import Dashboard from "./pages/Dashboard";
@@ -12,9 +14,18 @@ import AppLayout from "./ui/AppLayout";
 
 import PageNotFound from "./pages/PageNotFound";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0
+    }
+  }
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -31,7 +42,7 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,16 +1,19 @@
+import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 function Cabins() {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img
-        alt="cabin"
-        src="https://xrxvkratggkrgwcgqcoo.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"
-      />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
