6f97014948300993c6391b5ca94dd03652816410
yuriy05 <dilayyura@gmail.com>
2024-11-23 01:40:01 +0200

    feat: client side sorting

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -19,14 +19,20 @@ function CabinTable() {
   }
 
   if (filterValue === "no-discount") {
-    filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+    filteredCabins = cabins?.filter(cabin => cabin.discount === 0);
   }
 
   if (filterValue === "with-discount") {
-    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+    filteredCabins = cabins?.filter(cabin => cabin.discount > 0);
   }
 
-  console.log(filterValue);
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins?.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
 
   if (isLoading) return <Spinner />;
 
@@ -42,7 +48,7 @@ function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={cabin => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
--- src/ui/Select.jsx
+++ src/ui/Select.jsx
@@ -4,7 +4,7 @@ const StyledSelect = styled.select`
   font-size: 1.4rem;
   padding: 0.8rem 1.2rem;
   border: 1px solid
-    ${(props) =>
+    ${props =>
       props.type === "white"
         ? "var(--color-grey-100)"
         : "var(--color-grey-300)"};
@@ -13,3 +13,17 @@ const StyledSelect = styled.select`
   font-weight: 500;
   box-shadow: var(--shadow-sm);
 `;
+
+function Select({ options, onChange, ...props }) {
+  return (
+    <StyledSelect onChange={onChange} {...props}>
+      {options.map(option => (
+        <option value={option.value} key={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </StyledSelect>
+  );
+}
+
+export default Select;
