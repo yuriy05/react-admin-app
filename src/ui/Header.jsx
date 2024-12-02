4cc9bd0c72752d504066e1ca4ce6d12718e5b2dd
yuriy05 <dilayyura@gmail.com>
2024-12-02 23:06:42 +0200

    styling header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,17 +1,25 @@
 import { styled } from "styled-components";
 
 import Logout from "../features/authentication/Logout";
+import HeaderMenu from "./HeaderMenu";
+import UserAvatar from "../features/authentication/UserAvatar";
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-greay-100);
+
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+  justify-content: flex-end;
 `;
 
 function Header() {
   return (
     <StyledHeader>
-      <Logout />
+      <UserAvatar />
+      <HeaderMenu />
     </StyledHeader>
   );
 }
